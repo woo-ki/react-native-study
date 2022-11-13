d774b91d3328c11a4036594b02fb0e567222b30d
김영욱 <ziziza93@naver.com>
2022-11-13 20:38:59 +0900

    기본 페이지 구성

---
+++ src/pages/Complete.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const Complete = () => {
+    return (
+        <View>
+            <Text>완료</Text>
+        </View>
+    );
+};
+
+export default Complete;
---
+++ src/pages/Orders.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const Orders = () => {
+    return (
+        <View>
+            <Text>주문</Text>
+        </View>
+    );
+};
+
+export default Orders;
