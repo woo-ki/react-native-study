7951229c7529504e6441c3ee198a343b33774b72
김영욱 <ziziza93@naver.com>
2022-11-19 10:14:16 +0900

    웹소켓 설정

--- AppInner.tsx
+++ AppInner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Orders from "./src/pages/Orders";
 import Delivery from "./src/pages/Delivery";
 import Settings from "./src/pages/Settings";
@@ -9,6 +9,7 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {useSelector} from "react-redux";
 import {RootState} from "./src/store/reducer";
+import useSocket from "./src/hooks/useSocket";
 
 export type LoggedInParamList = {
     Orders: undefined;
@@ -27,6 +28,30 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppInner = () => {
     const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+    const [socket, disconnect] = useSocket();
+
+    useEffect(() => {
+        const helloCallback = (data: any) => {
+            console.log(data);
+        };
+        if (socket && isLoggedIn) {
+            console.log(socket);
+            socket.emit("login", "");
+            socket.on("hello", helloCallback);
+        }
+        return () => {
+            if (socket) {
+                socket.off("hello", helloCallback);
+            }
+        };
+    }, [isLoggedIn, socket]);
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            console.log("!isLoggedIn", !isLoggedIn);
+            disconnect();
+        }
+    }, [isLoggedIn, disconnect]);
     return (
         <NavigationContainer>
             {isLoggedIn ? (
